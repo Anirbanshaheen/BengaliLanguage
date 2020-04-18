d56ed44a6f646a74991c697d99de8be1b1836848
Anirbanshaheen <anirbanshaheen97@gmail.com>
2020-04-18 19:49:56 +0600

    Adding feature

---
+++ app/src/main/java/com/example/bengalilanguage/BarisalActivity.java
@@ -0,0 +1,69 @@
+package com.example.bengalilanguage;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class BarisalActivity extends AppCompatActivity {
+    private TabLayout tabLayoutBarisal;
+    private ViewPager2 viewPager2Barisal;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barisal);
+
+        tabLayoutBarisal = findViewById(R.id.barisalTabLayout);
+        viewPager2Barisal = findViewById(R.id.viewPagerBarisal);
+
+        viewPager2Barisal.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        String[] text = {"Relation", "Food", "Phrase"};
+
+        public MyPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            if (position == 0) {
+                return new FragmentRelationBarisal();
+            }
+            if (position == 1) {
+                return new FragmentFoodBarisal();
+            }
+            if (position == 2) {
+                return new FragmentPhraseBarisal();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return text.length;
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return text[position];
+        }
+    }
+}
---
+++ app/src/main/java/com/example/bengalilanguage/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.bengalilanguage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textViewBarisal;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textViewBarisal = findViewById(R.id.barisalMA);
+
+        textViewBarisal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,BarisalActivity.class));
+                finish();
+            }
+        });
+
+
+    }
+}
