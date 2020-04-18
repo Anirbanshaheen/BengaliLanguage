7f5591494e50bf378ce0e6e1c02a2c3e0ba22598
Shahidullah Al Anik <me.alanik95@gmail.com>
2020-04-18 20:15:04 +0600

    fixed ViewPager

--- app/src/main/java/com/example/bengalilanguage/BarisalActivity.java
+++ app/src/main/java/com/example/bengalilanguage/BarisalActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.app.Activity;
@@ -17,8 +18,7 @@ import com.google.android.material.tabs.TabLayout;
 
 public class BarisalActivity extends AppCompatActivity {
     private TabLayout tabLayoutBarisal;
-    private ViewPager2 viewPager2Barisal;
-
+    private ViewPager viewPager2Barisal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +27,17 @@ public class BarisalActivity extends AppCompatActivity {
 
         tabLayoutBarisal = findViewById(R.id.barisalTabLayout);
         viewPager2Barisal = findViewById(R.id.viewPagerBarisal);
-
-        viewPager2Barisal.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager(), tabLayoutBarisal.getTabCount());
+        viewPager2Barisal.setAdapter(myPagerAdapter);
     }
 
-    public class MyPagerAdapter extends FragmentPagerAdapter {
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
 
         String[] text = {"Relation", "Food", "Phrase"};
 
-        public MyPagerAdapter(@NonNull FragmentManager fm) {
-            super(fm);
+        public MyPagerAdapter(@NonNull FragmentManager fm, int b) {
+            super(fm, b);
         }
-
 
         @NonNull
         @Override
